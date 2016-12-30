fd4ad77d3dd3f1c7a0f516a3e901c31c0edf457a
LaCruz <lacruz@gmx.net>
2016-12-30 05:12:20 +0100

    working common n2awifi with bugs

---
+++ system_prop.mk
@@ -0,0 +1,23 @@
+#
+# Common system properties for exynos5420
+#
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sapa.jack.master=alsa \
+    ro.hdcp2.rx=tz
+
+# Open GL
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196609
+
+# Randomly from stock
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.gprsclass=10 \
+    ro.ril.hsxpa=1 \
+    ro.sec.fle.encryption=true \
+    ro.secwvk=220
+
+# LockScreen Rotation
+PRODUCT_PROPERTY_OVERRIDES += \
+    lockscreen.rot_override=true
